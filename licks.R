9547466066c7500052ca8422fa67f3ce214209e4
nicolasluarte <l.luarte@uc.cl>
2021-04-18 00:37:49 -0400

    worked some functions

--- licks.R
+++ licks.R
@@ -45,8 +45,7 @@ tagEvents <- function(dataRaw,
     mutate(msFromEvent = countMsFromEvent(isEvent, ms)) %>% 
     mutate(isTimeOut = isTimeOut(msFromEvent, 20000, !!sym(ECUM)))
   dataRaw <- dataRaw %>% group_by(isLick, !!sym(ARDUINO), !!sym(SPOUT)) %>% 
-    mutate(ILI = abs(ms - lag(ms, default = first(ms)))) %>% 
-    mutate(ILIRoll = rollmean(ILI, 100, fill = NA))
+    mutate(ILI = abs(ms - lag(ms, default = first(ms))))
   return(dataRaw)
 }
 
@@ -82,19 +81,110 @@ isTimeOut <- function(countMsFromEvent, timeOut, eventsCum){
   return(isTimeOut)
 }
 
+latencyToFirstLick <- function(){
+  # to any spout
+  
+}
+
+## este es el ejemplo de la funcion
+nLicks <- function(dataFrame){
+  dataFrame$var1 = c(3,3,3)
+  return(dataFrame)
+}
+
+df <- data.frame(var1 = c(3, 2, 1))
+df2 <- nLicks(df)
+
+# nLicksInTimeOut
+# @isLick: a vector of 0 and 1 indicating if a lick was detected or not
+# @isTimeout: a vector of 0 and 1 indicating if it was time out or not
+nLicksInTimeOut <- function(isLick, isTimeout){
+	# check for proper vector format
+	if (!all(isLick %in% c(1, 0))) { stop("isLick vector not correct")}
+	if (!all(isTimeout %in% c(1, 0))) { stop("isTimeOut vector not correct")}
+	if (!length(isLick) == length(isTimeout)) { stop("Vectors need to be of the same size")}
+	isLickVector <- as.vector(isLick)
+	isTimeoutVector <- as.vector(isTimeout)
+	v <- isLickVector * isTimeoutVector
+	returnList <- list(licksInTimeout = v,
+			   nLicksInTimeout = sum(v),
+			   nLicksOutOfTimeout = sum(isLickVector) - sum(v))
+	return(returnList)
+}
+
+# nEvents
+# @eventsCum: a vector which contains the cumulative count if events
+nEvents <- function(eventsCum){
+	if(!is.numeric(eventsCum)) { stop("Vector should be numeric")}
+	return(list(nEvents = max(as.vector(eventsCum))))
+}
+
+# binsMs
+# @ms: a vector containg the ms from start, starting from 0
+# @windowsSize: how many ms per bin
+# return: a vector containing the bins of ms, according to range
+binsMs <- function(ms, windowsSize){
+	if(!is.numeric(ms)) { stop("Vector should be numeric")}
+	return(list(msBins = cumsum(ms %% windowsSize == 0)))
+}
+
+tagBurst <- function(){
+  
+}
+
+tagPauses <- function(){
+  
+}
+
+licksPerMs <- function(){
+}
+
 path <- "/home/nicoluarte/drive/Resultados OBJ 4/PR RATONES CANULADOS MARZO 2021 (206 al 216)/PR"
-files <- dir(path, pattern = "*.txt")
+files <- dir(path, pattern = "*.events_.txt")
 dfList <- files %>%
   map(~ parseLickometerRaw(file.path(path, .),
                            headers = c("date", "pc_time", "secStart", "msStart",
                                    "arduino", "spout", "licksCum", "eventsCum"),
                            dataTypes = c("character", "character", "integer", "integer", "factor", "factor", "integer", "integer")))
+
+
+## AQUI SE CARGA EL ARCHIVO
+dataFrame <- parseLickometerRaw("aqui va nombre del archivo.txt",
+                           headers = c("date", "pc_time", "secStart", "msStart",
+                                   "arduino", "spout", "licksCum", "eventsCum"),
+                           dataTypes = c("character", "character", "integer", "integer", "factor", "factor", "integer", "integer"))
+
 # check for empty datasets
 dfList <- purrr::keep(dfList, ~ nrow(.x) > 0)
 
 dfListTagged <- dfList %>% map(~ tagEvents(.))
 dfTagged <- dfList %>% map_dfr(~ tagEvents(.))
 
+# break to categorize ms
+dfTagged$cat <- cut(dfTagged$ms, 
+                   breaks=c(-Inf, 1500000, 3000000, Inf), 
+                   labels=c("inicio","medio","fin"))
+
+
+
+
+
+
+## N of licks
+
+dfTagged %>% ggplot(aes(x = ms, y = licksCum, color = spout)) +
+  geom_smooth()
+
+dfTagged %>% ggplot(aes(x = ms, y = ILI, color = as.factor(isTimeOut))) +
+  geom_smooth(method = "lm")
+
+dfTagged %>% ggplot(aes(ILI, fill = as.factor(eventsCum))) +
+  geom_density(alpha = 0.5) + xlim(0, 500) +
+  theme_bw()
+
+dfTagged %>% filter(isLick == 1) %>% ggplot(aes(ILI, fill = as.factor(cat))) +
+  geom_density(position = "stack", adjust = 2) + xlim(0, 2500) +
+  theme_bw()
 
 
 
